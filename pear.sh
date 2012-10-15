88ecba43a519010ab5004002311e8ccea1a652bc
Arne Blankerts <Arne@Blankerts.de>
2012-10-15 15:14:35 +0200

    Additional work for issue #14

--- pear.sh
+++ pear.sh
@@ -3,7 +3,7 @@ rm -f fDOMDocument*.tgz
 mkdir -p tmp/TheSeer/fDOMDocument
 cp -r src/* tmp/TheSeer/fDOMDocument
 phpab -o tmp/TheSeer/fDOMDocument/autoload.php -b src src
-cp package.xml tmp
+cp package.xml LICENSE README.md tmp
 cd tmp
 pear package
 mv fDOMDocument*.tgz ..
