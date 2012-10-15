03df0640ddfba6b78c84e7c6d20f190294a77c67
Arne Blankerts <Arne@Blankerts.de>
2012-10-15 15:32:49 +0200

    Adding tests to pear package as requested in issue #14

--- pear.sh
+++ pear.sh
@@ -3,7 +3,8 @@ rm -f fDOMDocument*.tgz
 mkdir -p tmp/TheSeer/fDOMDocument
 cp -r src/* tmp/TheSeer/fDOMDocument
 phpab -o tmp/TheSeer/fDOMDocument/autoload.php -b src src
-cp package.xml LICENSE README.md tmp
+cp package.xml LICENSE README.md phpunit.xml.dist tmp
+cp -r tests tmp
 cd tmp
 pear package
 mv fDOMDocument*.tgz ..
